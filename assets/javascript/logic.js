e0f2b2e68ac329d24f751dc04ea13c1eabfb201d
CCastro <chris502castro@gmail.com>
2018-11-21 22:15:09 -0700

    Books button switched for news button; all logic (including news button) seems to be working properly. Now needs styling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,6 @@
 // Array of topics to be updated with user-input
 var topic = ["rick and morty", "simpsons", "family guy", "big mouth", "spongebob", "rocket power", "futurama", "disenchanted"];
+
 // Function to list & re-list (if user inputs string) topics to be clicked
 function listTopics(topic) {
     $("#topic-holder").html("")
@@ -7,8 +8,10 @@ function listTopics(topic) {
         $("#topic-holder").append($("<button>").addClass("btn btn-outline-primary ind-topic").attr("id", "item-" + i).text(topic[i]));
     }
 }
+
 // Lists original topics
 listTopics(topic);
+
 // Function to grab user input, push it into the topic array, then call on listTopics function w/ new topic array
 $("#search").on("click", function () {
     topic.push($("#termField").val());
@@ -16,7 +19,7 @@ $("#search").on("click", function () {
     $("#termField").val("")
 })
 
-// function that, once the user clicks a topic, asks the user to pick a type of information for that topic (gifs, omdb, or books), then calls on the appropriate function
+// function that, once the user clicks a topic, asks the user to pick a type of information for that topic (gifs, omdb, or news), then calls on the appropriate function
 $("body").on("click", "button.ind-topic", function (event) {
     var id1 = (event.currentTarget.attributes.id.value).split("-")[1];
     id1 = parseInt(id1);
@@ -25,15 +28,15 @@ $("body").on("click", "button.ind-topic", function (event) {
 
 // function takes in the id from the topic button pushed, and waits until user picks a format to call the corresponding function which pulls api info
 function pickFormat(id1) {
-    alert("You must select a format. Please press the gif, movie, or book button");
+    alert("You must select a format. Please press the gif, movie, or news button");
     $(".btn-block").on("click", function (event) {
         var id2 = event.currentTarget.attributes.id.value
         if (id2 === "gifButton") {
             gifPull(id1);
         } else if (id2 === "movieButton") {
             moviePull(id1);
-        } else if (id2 === "bookButton") {
-            bookPull(id1);
+        } else if (id2 === "newsButton") {
+            newsPull(id1);
         } else {
             $(".container").html("error");
         }
@@ -51,7 +54,7 @@ function gifPull(id1) {
         $("#info-holder").html("");
         var shortHand = result.data;
         console.log(shortHand);
-        for (i=0;i<shortHand.length;i++) {
+        for (i = 0; i < shortHand.length; i++) {
             console.log(shortHand[i].embed_url)
             $("#info-holder").append($("<img>").attr("src", shortHand[i].images.fixed_height.url))
         }
@@ -82,7 +85,22 @@ function moviePull(id1) {
     })
 }
 
-// Displays book info for the selected topic
-function bookPull(id1) {
-    console.log("book run")
+// Display news info for the selected topic
+function newsPull(id1) {
+    var condensedTopic = topic[id1].split('').join('').toLowerCase();
+    var url = "https://content.guardianapis.com/search?api-key=1e735112-157f-4f28-8980-fb5bdade6370&q=" + condensedTopic;
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: "GET",
+    }).then(function (result) {
+        $("#info-holder").html("");
+        var shortHand = result.response.results
+        console.log(shortHand);
+        for (i = 0; i < 5; i++) {
+            $("#info-holder").append($("<h3>").attr("id", "title").text(shortHand[i].webTitle))
+            $("#info-holder").append($("<p>").attr("id", "webPubDate").text("Web Publication Date: " + shortHand[i].webPublicationDate))
+            $("#info-holder").append($("<a>").attr("href", "https://www.theguardian.com/tv-and-radio/2018/may/11/rick-and-morty-renewed-for-70-more-episodes-adult-swim").attr("id", "url").attr("target", "_blank").text("Link"))
+        }
+    })
 }
